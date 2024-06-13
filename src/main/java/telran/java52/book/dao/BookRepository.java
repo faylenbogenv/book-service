e38fb410442d0c1361f18594c36f3e874f723d6f
faylenbogenv <faylenbogenv@gmail.com>
2024-06-13 20:53:45 +0300

    EntityManager first commit

--- src/main/java/telran/java52/book/dao/BookRepository.java
+++ src/main/java/telran/java52/book/dao/BookRepository.java
@@ -1,21 +1,34 @@
 package telran.java52.book.dao;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Stream;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import telran.java52.book.model.Author;
 import telran.java52.book.model.Book;
+import telran.java52.book.model.Publisher;
 
-public interface BookRepository extends JpaRepository<Book, String> {
+public interface BookRepository {
 
-//	Stream<Book> findByAuthorsName(String authorName);
+ Stream<Book> findByAuthorsName(String authorName);
 	
-//	Stream<Book> findByPublisherPublisherName(String publisherName);
+	Stream<Book> findByPublisherPublisherName(String publisherName);
 	
 	void deleteByAuthorsName(String name);
+
+	boolean existsById(String isbn);
+
+	Book save(Book book);
+
+	Optional<Book> findById(String isbn);
+
+	void deleteById(String isbn);
+	
+	
 	
 //	@Query("select b from Book b join b.authors a where a.name = :author")
 //	List<Book> findBooksByAuthor(@Param("author") String author);
